8ce807eed4ffc43f6b678dc24f7a7d1f5a99cd11
paragao <paragao01@gmail.com>
2020-03-26 15:01:34 -0300

    Alteracao do arquivo index

--- deployHomologacao.sh
+++ deployHomologacao.sh
@@ -3,7 +3,7 @@
 git add .
 git commit -m "Alteracao do arquivo index"
 git push origin master
-git push myserver master:master
+git push serverHomologacao master:master
 echo .
 echo .
 echo Feito deploy da aplicacao
